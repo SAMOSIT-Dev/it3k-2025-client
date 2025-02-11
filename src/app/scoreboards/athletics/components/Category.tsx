f8f7d79d5afebe4ac1b8da99bc5deaccacef0ced
Tonpor <88127284+Nannapatx@users.noreply.github.com>
2025-02-12 02:02:47 +0700

    :sparkles: create category component with mocked data and scoreboard page

---
+++ src/app/scoreboards/athletics/components/Category.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import styles from '@/styles/scoreboards/athletics/category.module.css'
+
+const Category: React.FC = () => {
+  const category = [
+    {
+      id: 1,
+      name: 'วิ่ง 100 เมตร หญิง'
+    },
+    {
+      id: 2,
+      name: 'วิ่ง 100 เมตร ชาย'
+    },
+    {
+      id: 3,
+      name: 'วิ่ง 4x100 เมตร หญิง'
+    },
+    {
+      id: 4,
+      name: 'วิ่ง 4x100 เมตร ชาย'
+    }
+  ]
+  return (
+    <>
+      <div>
+        <ul className={styles.container}>
+          {category.map((item, id) => (
+            <li
+              className={`text-white mr-2 border ${styles.category_button}`}
+              key={id}>
+              <button>{item.name}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  )
+}
+export default Category
---
+++ src/app/scoreboards/page.tsx
@@ -0,0 +1,10 @@
+export default function Scoreboards() {
+  return (
+    <div>
+      <h1>Scoreboard</h1>
+      <p>
+        The Scoreboard page is coming soon!
+      </p>
+    </div>
+  );
+}

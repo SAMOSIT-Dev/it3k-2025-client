4d9915206e568fb4ac18b2c4b0d91fefec3e9734
Tonpor <88127284+Nannapatx@users.noreply.github.com>
2025-02-12 02:02:47 +0700

    :sparkles: create category component with mocked data and scoreboard page

---
+++ src/app/scoreboards/athletics/page.tsx
@@ -0,0 +1,13 @@
+import Category from './components/Category'
+
+export default function Athletics() {
+  return (
+    <div className="min-h-screen w-full bg-default-gradient px-36 py-20 text-white">
+      <div className="mb-4">
+        <h1>Athletics</h1>
+        <p>The Athletics page is coming soon!</p>
+      </div>
+      <Category />
+    </div>
+  )
+}

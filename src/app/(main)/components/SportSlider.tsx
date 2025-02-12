c3ee2788cbeb7c7a2257b692cc0f855829b0e5ed
jetta <jettaphoom@gmail.com>
2025-02-12 12:56:42 +0700

    [add] sport-slider component

---
+++ src/app/(main)/components/SportSlider.tsx
@@ -0,0 +1,60 @@
+'use client'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation } from 'swiper/modules'
+import Image from 'next/image'
+import { Icon } from '@iconify/react'
+
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/navigation'
+import Link from 'next/link'
+
+interface SportSliderProps {
+  title: string
+  sportLists: { name: string; route: string; icon: string }[]
+}
+
+const SportSlider = ({ title, sportLists }: SportSliderProps) => {
+  return (
+    <div className="px-8 md:px-36 py-4 sm:py-8">
+      <h1 className="text-white text-lg md:text-2xl font-bold font-Prompt text-center">
+        {title}
+      </h1>
+      <Swiper
+        slidesPerView={4}
+        loop={true}
+        navigation={{
+          prevEl: '.custom-prev',
+          nextEl: '.custom-next'
+        }}
+        modules={[Navigation]}>
+        {sportLists.map((sport, index) => (
+          <SwiperSlide key={index}>
+            <Link
+              href={sport.route}
+              className="flex items-center justify-center flex-col">
+              <Image
+                src={sport.icon}
+                alt={sport.name}
+                height={200}
+                width={200}
+              />
+              <p className="text-white text-[14px] sm:text-xl text-center md:text-2xl font-bold font-Prompt ">
+                {sport.name}
+              </p>
+            </Link>
+          </SwiperSlide>
+        ))}
+
+        <button className="custom-prev hidden lg:flex absolute left-2 top-1/2 transform -translate-y-1/2 text-white text-4xl p-2 rounded-full shadow-md z-10">
+          <Icon icon="bxs:left-arrow" />
+        </button>
+        <button className="custom-next hidden lg:flex absolute right-2 top-1/2 transform -translate-y-1/2 text-white text-4xl p-2 rounded-full shadow-md z-10">
+          <Icon icon="bxs:right-arrow" />
+        </button>
+      </Swiper>
+    </div>
+  )
+}
+
+export default SportSlider
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import SportSlider from './(main)/components/SportSlider'
+import { scheduleSports, scoreboardSports } from './(main)/data/sportData'
 
 export default function Home() {
   return (
@@ -7,14 +9,15 @@ export default function Home() {
       {/* Podium Section By Icde */}
 
       {/* Sport Scoreboard Slider By Gun*/}
-
+      <SportSlider title="รายงานการแข่งขัน" sportLists={scoreboardSports} />
       {/* Podium Scoreboard By Ice */}
 
       {/* Live Schedule By C */}
 
       {/* Full detail sport schedule slider By Gun*/}
+      <SportSlider title="กำหนดการแต่ละกีฬา" sportLists={scheduleSports} />
 
       {/* Sponsers */}
     </main>
-  );
+  )
 }

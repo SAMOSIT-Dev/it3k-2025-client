2e090bf43c6554c1eb7d08fc9e061b251bdd1e01
KINNOM-Cyber <kinnom254097@gmail.com>
2025-02-14 21:19:54 +0700

    ✨ (page): add schedule data for each sport

--- src/app/schedule/(pages)/badminton/page.tsx
+++ src/app/schedule/(pages)/badminton/page.tsx
@@ -1,3 +1,30 @@
+import { useId } from 'react'
+import PageTitle from '../../components/PageTitle'
+import ScheduleCard from '../../components/ScheduleCard'
+import { badmintonScheduleData } from '../../scheduleData'
+
 export default function BadmintonSchedulePage() {
-  return <div>Badmintion Schedule Page</div>
+  return (
+    <div className="w-screen font-Prompt h-screen overflow-scroll bg-black-300 fixed inset-0">
+      <header className="">
+        <div className="w-[1038.35px] mx-auto">
+          <PageTitle title="badminton" />
+        </div>
+      </header>
+      <main className="my-[100px] h-auto min-h-0 mx-auto w-[1038.35px] relative z-20">
+        {badmintonScheduleData.map((type) => (
+          <div key={type.title}>
+            <h1 className="text-5xl flex justify-center font-bold text-center text-white mx-auto my-[86px]">
+              {type.title}
+            </h1>
+            <div className="font-Prompt flex flex-col justify-center m-auto space-y-[42px]">
+              {type.scheduleData.map((data) => (
+                <ScheduleCard key={useId()} scheduleData={data} />
+              ))}
+            </div>
+          </div>
+        ))}
+      </main>
+    </div>
+  )
 }
--- src/app/schedule/(pages)/tradition-game/page.tsx
+++ src/app/schedule/(pages)/tradition-game/page.tsx
@@ -1,4 +1,23 @@
+import { useId } from 'react'
+import ScheduleCard from '../../components/ScheduleCard'
+import { traditionGameScheduleData } from '../../scheduleData'
+import PageTitle from '../../components/PageTitle'
+
 export default function TraditionGameSchedulePage() {
-    return <div>Tradition Game Schedule Page</div>
-  }
-  
+  return (
+    <div className="w-screen h-screen overflow-scroll bg-black-300 fixed inset-0">
+      <header className="">
+        <div className="w-[1038.35px] mx-auto">
+          <PageTitle title="tradition_game" />
+        </div>
+      </header>
+      <main className="my-[100px] h-auto min-h-0 mx-auto w-[1038.35px] relative z-20">
+        <div className="font-Prompt flex flex-col justify-center m-auto space-y-[42px]">
+          {traditionGameScheduleData.map((data) => (
+            <ScheduleCard key={useId()} scheduleData={data} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
+}

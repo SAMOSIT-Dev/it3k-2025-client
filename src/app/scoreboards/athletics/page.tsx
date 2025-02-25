465c826142403344e83f84fd1f574ee6f1c2dc77
Tonpor <88127284+Nannapatx@users.noreply.github.com>
2025-02-25 20:38:43 +0700

    :recycle: refactor code

--- src/app/scoreboards/athletics/page.tsx
+++ src/app/scoreboards/athletics/page.tsx
@@ -7,16 +7,17 @@ import { useRouter } from 'next/navigation'
 export default function Athletics() {
   const router = useRouter()
   return (
-    <div className="min-h-screen w-full bg-black-300 p-10 lg:p-20 text-white">
-      <button
-        className="mb-8 flex flex-row items-center lg:-ml-4 md:-ml-2 -ml-1"
-        onClick={() => router.push('/scoreboards')}>
+    <div className="font-Prompt min-h-screen w-full bg-black-300 p-10 lg:p-20 text-white">
+      <div
+        className="mb-2 md:mb-8 flex flex-row items-center lg:-ml-4 md:-ml-2 -ml-1">
+          <button onClick={() => router.push('/scoreboards')}>
         <Icon
           icon={'solar:alt-arrow-left-bold'}
-          className="lg:text-6xl md:5xl text-4xl p-0 cursor-pointer"
+          className="lg:text-6xl md:5xl text-3xl p-0 cursor-pointer"
         />
-        <h1 className="text-3xl lg:text-4xl font-bold">กรีฑา</h1>
-      </button>
+        </button>
+        <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold">กรีฑา</h1>
+      </div>
       <Category />
     </div>
   )

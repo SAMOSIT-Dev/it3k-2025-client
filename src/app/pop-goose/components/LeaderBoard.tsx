a1b623bcb16ed5c42541b9b0a8a05672231b4d7f
jetta <jettaphoom@gmail.com>
2025-02-26 21:58:04 +0700

    ✨ feat: prevent auto clicker & delay 0.5s time sending request

--- src/app/pop-goose/components/LeaderBoard.tsx
+++ src/app/pop-goose/components/LeaderBoard.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useMemo } from 'react'
+import CountUp from 'react-countup'
 
 interface LeaderboardEntry {
   rank: string
@@ -20,14 +21,21 @@ const Leaderboard = ({ leaderboardData }: LeaderboardProps) => {
   }, [leaderboardData])
 
   return (
-    <div className="bg-[#ECECEC] w-[365px] h-[30px] flex items-center justify-center sm:h-[60px] sm:w-[620px] md:w-[750px] lg:w-[850px] p-2 sm:p-4 rounded-full">
+    <div className="bg-[#ECECEC] w-[365px] h-[30px] flex items-center justify-center sm:h-[60px] sm:w-auto p-2 sm:p-4 rounded-full">
       <ul className="flex flex-row items-center justify-evenly gap-2 sm:gap-6 font-PressStart2P">
         {sortedLeaderboard.map((team, index) => (
           <li
             key={team.rank}
             className="flex items-center justify-center sm:gap-2 text-[6px] sm:text-[12px] xl:text-[14px]">
             {index + 1}.<span>{team.university}</span>
-            <span className="text-[#3A5FE5]">{team.clicks}</span>
+            <span className="text-[#3A5FE5]">
+              <CountUp
+                start={Number(team.clicks) - 100}
+                end={Number(team.clicks)}
+                duration={0.8}
+                separator=","
+              />
+            </span>
           </li>
         ))}
       </ul>
--- src/app/pop-goose/page.tsx
+++ src/app/pop-goose/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Image from 'next/image'
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import defaultGoose from '../../../public/images/pop_goose/default_goose.png'
 import popGoose from '../../../public/images/pop_goose/pop_goose.png'
 import gooseShadow from '../../../public/images/pop_goose/goose-shadow.png'
@@ -24,33 +24,79 @@ const PopGoosePage = () => {
   const [clickCount, setClickCount] = useState(0)
   const [isPopped, setIsPopped] = useState(false)
   const [leaderboardData, setLeaderboardData] = useState<LeaderboardEntry[]>([])
+  const popSound = '/audio/popSound.mp3'
+  const audioRef = useRef<HTMLAudioElement | null>(null)
+  const lastClickTime = useRef(0)
+  const clickBuffer = useRef(0)
+  const emitTimeout = useRef<NodeJS.Timeout | null>(null)
+  const clickTimestamps = useRef<number[]>([])
 
   useEffect(() => {
     const socket = initSocket()
 
-    socket.on('connect', () => {
-      console.log('Connected: ', socket.id)
-    })
+    socket.on('connect', () => {})
 
     socket.on('updateLeaderboard', (data) => {
-      console.log('Retrieve latest leaderboard data', data)
       setLeaderboardData(data)
     })
 
     socket.emit('requestLeaderboard')
 
     return () => {
+      if (emitTimeout.current) {
+        clearTimeout(emitTimeout.current)
+      }
       socket.disconnect()
     }
   }, [])
 
+  const emitClicksToServer = useCallback(() => {
+    if (clickBuffer.current > 0 && university) {
+      const socket = initSocket()
+      socket.emit('click', { university, clicks: clickBuffer.current })
+      clickBuffer.current = 0
+    }
+
+    emitTimeout.current = null
+  }, [university])
+
   const handlePress = useCallback(() => {
+    const now = Date.now()
+    const timeSinceLastClick = now - lastClickTime.current
+
+    if (timeSinceLastClick < 10) {
+      setIsModal(true)
+      console.error('Error 403: Big Brain Not Found.')
+      return
+    }
+    clickTimestamps.current = clickTimestamps.current.filter(
+      (timestamp) => now - timestamp < 1000
+    )
+
+    if (clickTimestamps.current.length >= 40) {
+      setIsModal(true)
+      console.error('Error 403: Big Brain Not Found.')
+      return
+    }
+
+    clickTimestamps.current.push(now)
+    lastClickTime.current = now
+
     setClickCount((prev) => prev + 1)
     setIsPopped(true)
 
-    const socket = initSocket()
-    socket.emit('click', { university, clicks: 1 })
-  }, [university])
+    if (typeof window !== 'undefined') {
+      audioRef.current = new Audio(popSound)
+      audioRef.current.currentTime = 0
+      audioRef.current.play()
+    }
+
+    clickBuffer.current += 1
+
+    if (!emitTimeout.current) {
+      emitTimeout.current = setTimeout(emitClicksToServer, 500)
+    }
+  }, [emitClicksToServer])
 
   const handleRelease = useCallback(() => {
     setIsPopped(false)
@@ -63,13 +109,13 @@ const PopGoosePage = () => {
 
   return (
     <section
-      className={`bg-[#9FC5E8] bg-gameBgSm sm:bg-gameBgMd xl:bg-gameBg bg-cover bg-center bg-no-repeat flex items-center justify-center min-h-screen py-4 select-none  ${styles['no-select']}`}>
+      className={`bg-[#9FC5E8] relative bg-gameBgSm sm:bg-gameBgMd xl:bg-gameBg bg-cover bg-center bg-no-repeat flex items-center justify-center min-h-screen py-4 select-none  ${styles['no-select']}`}>
       {isModal && <UniversitySelectModal onSubmit={handleUniSelect} />}
 
       <Link href={'/'}>
         <Icon
           icon={'bxs:left-arrow'}
-          className="text-[#e9000080] absolute top-[50px] sm:top-[80px] sm:left-[80px] text-2xl sm:text-5xl xl:text-6xl"
+          className="text-[#e9000080] absolute top-[50px] sm:top-[50px] sm:left-[80px] text-2xl sm:text-5xl xl:text-6xl"
         />
       </Link>
       <div className="flex flex-col h-[600px] sm:h-[700px] xl:h-full items-center justify-between space-y-2">
@@ -93,11 +139,7 @@ const PopGoosePage = () => {
             draggable="false"
           />
 
-          <div
-            onMouseDown={handlePress}
-            onMouseUp={handleRelease}
-            onTouchStart={handlePress}
-            onTouchEnd={handleRelease}>
+          <div onPointerDown={handlePress} onPointerUp={handleRelease}>
             <Image
               src={isPopped ? popGoose : defaultGoose}
               alt="Pop Goose"

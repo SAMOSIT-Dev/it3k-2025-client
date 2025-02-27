ee02ea968c1f82a33bbfaf4db6e11bbb204448b8
BoolinBood <napat.jins@mail.kmutt.ac.th>
2025-02-27 16:29:43 +0700

    🔧(socket): add configurations

--- src/app/pop-goose/utils/socket.ts
+++ src/app/pop-goose/utils/socket.ts
@@ -4,12 +4,15 @@ let socket: Socket
 
 export const initSocket = (): Socket => {
   if (!socket && typeof window !== 'undefined') {
-    const socketServerUrl = process.env.NEXT_PUBLIC_SOCKET_SERVER_URL || 'http://localhost/popcat/socket/'
+    const socketServerUrl = process.env.NEXT_PUBLIC_SOCKET_SERVER_URL || 'http://localhost'
+    const socketServerPath = process.env.NEXT_PUBLIC_SOCKET_SERVER_URL_PATH || '/popcat/socket/'
     if (!socketServerUrl) {
       throw new Error('Socket server URL is not defined')
     }
     socket = io(socketServerUrl, {
-      transports: ['websocket']
+      transports: ['websocket'], // Force WebSocket connection
+      withCredentials: true,
+      path: socketServerPath
     })
   }
   if (!socket) {

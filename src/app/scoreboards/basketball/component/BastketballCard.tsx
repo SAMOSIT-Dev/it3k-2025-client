6ba6f3b2b6a20d95e7e02369a7fb2c8f00ab5723
Chanakarn Limprasertsiri <minklim47@gmail.com>
2025-02-28 10:56:05 +0700

    🎨 fix responsive

--- src/app/scoreboards/basketball/component/BastketballCard.tsx
+++ src/app/scoreboards/basketball/component/BastketballCard.tsx
@@ -14,7 +14,7 @@ interface IBasketballCard {
 
 const BasketBallCard = ({ data }: { data: IBasketballCard }) => {
   return (
-    <div className="relative  max-w-[375px] w-[90%] h-[70px]  sm:max-w-[600px] sm:h-[120px] lg:max-w-[1000px] lg:h-[200px]">
+    <div className="relative  max-w-[375px] w-[90%] h-[70px] sm:max-w-[600px] sm:h-[120px] lg:max-w-[1000px] lg:h-[200px]">
       <div className="absolute  left-[-15px] right-[-15px] h-full rounded-md border-[#E90000] border-[1px] bg-gradient-to-r from-[#E90000] to-[#F16322]"></div>
       <div className="absolute flex items-center justify-around w-full h-full p-3 rounded-md border-[#E90000] border-[1px] bg-black-300">
         <div className="flex flex-col text-[#E9E9E9] text-center text-[10px] sm:text-[16px] lg:text-[24px]">
---
+++ src/app/scoreboards/basketball/page.tsx
@@ -0,0 +1,80 @@
+'use client'
+
+import { Icon } from '@iconify/react/dist/iconify.js'
+import BasketBallCard from './component/BastketballCard'
+import { useRouter } from 'next/navigation'
+const BasketBall = () => {
+  const mock = [
+    {
+      id: 1,
+      teamA: 'KMUTT',
+      teamB: 'KMITL',
+      time: '17.00-20.00',
+      scoreAQ1: 32,
+      scoreAQ2: 32,
+      scoreBQ1: 0,
+      scoreBQ2: 0,
+      scoreAT: 102,
+      scoreBT: 0
+    },
+    {
+      id: 1,
+      teamA: 'KMUTT',
+      teamB: 'KMITL',
+      time: '17.00-20.00',
+      scoreAQ1: 32,
+      scoreAQ2: 32,
+      scoreBQ1: 0,
+      scoreBQ2: 0,
+      scoreAT: 102,
+      scoreBT: 0
+    },
+    {
+      id: 1,
+      teamA: 'KMUTT',
+      teamB: 'KMITL',
+      time: '17.00-20.00',
+      scoreAQ1: 32,
+      scoreAQ2: 32,
+      scoreBQ1: 0,
+      scoreBQ2: 0,
+      scoreAT: 102,
+      scoreBT: 0
+    },
+    {
+      id: 1,
+      teamA: 'KMUTT',
+      teamB: 'KMITL',
+      time: '17.00-20.00',
+      scoreAQ1: 32,
+      scoreAQ2: 32,
+      scoreBQ1: 0,
+      scoreBQ2: 0,
+      scoreAT: 102,
+      scoreBT: 0
+    }
+  ]
+  const router = useRouter()
+
+  return (
+    <div className=" flex flex-col w-full min-h-screen flex-1  items-center py-12  bg-black-300  space-y-2 sm:space-y-4 lg:space-y-8">
+      <div className="flex items-center w-[100%] ml-[10%] mb-5">
+        <button onClick={() => router.push('/scoreboards')}>
+          <Icon
+            icon={'solar:alt-arrow-left-bold'}
+            className="lg:text-6xl md:5xl text-4xl p-0 cursor-pointer text-white"
+          />
+        </button>
+        <h1 className="text-3xl text-white lg:text-4xl font-bold">
+          Basketball
+        </h1>
+      </div>
+      {/* <div className='flex flex-col items-center w-full h-full space-y-2 sm:space-y-4 lg:space-y-8'> */}
+        {mock.map((data, index) => {
+          return <BasketBallCard data={data} key={index} />
+        })}
+      {/* </div> */}
+    </div>
+  )
+}
+export default BasketBall

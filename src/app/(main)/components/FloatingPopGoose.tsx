0acbdfeaecad7050f2f39f9a36d6f8e929d55deb
BoolinBood <napat.jins@mail.kmutt.ac.th>
2025-03-02 19:52:30 +0700

    🔧(floating-popgoose): change font and change img to Image tag

--- src/app/(main)/components/FloatingPopGoose.tsx
+++ src/app/(main)/components/FloatingPopGoose.tsx
@@ -1,5 +1,6 @@
 'use client'
 import styles from '@/app/styles/floatingPopGoose.module.css'
+import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import { useState, useEffect } from 'react'
 
@@ -26,7 +27,7 @@ const FloatingPopGoose = () => {
   return (
     <div
       onClick={() => router.push('/pop-goose')}
-      className={`fixed bottom-4 right-4 flex items-center justify-center cursor-pointer
+      className={`fixed bottom-4 right-4 flex items-center justify-center cursor-pointer font-PressStart2P
                    w-16 h-16 md:w-28 md:h-28 lg:w-32 lg:h-32 rounded-full shadow-lg z-50
                    ${styles.floating} ${styles['gradient-bg']}`}>
       <div className="flex flex-col items-center justify-center">
@@ -35,7 +36,7 @@ const FloatingPopGoose = () => {
             POPGOOSE
           </span>
         )}
-        <img
+        <Image
           src="/images/pop.png"
           alt="Goose"
           className="w-10 h-10 md:w-16 md:h-16 lg:w-20 lg:h-20 mt-2"

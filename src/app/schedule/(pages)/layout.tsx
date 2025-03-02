6729ffc199642bd080fa2cfabc93e707b25cdc4c
KINNOM-Cyber <kinnom254097@gmail.com>
2025-03-02 18:51:48 +0700

    🔧 (page component): Add and fix hidden overflow in x axis

--- src/app/schedule/(pages)/layout.tsx
+++ src/app/schedule/(pages)/layout.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from 'react'
 
 export default function ScheduleLayout({ children }: { children: ReactNode }) {
-  return <div className="flex min-h-0 w-screen overflow-auto h-screen">{children}</div>
+  return (
+    <div className="flex w-auto overflow-auto overflow-x-hidden h-auto relative bg-black-300 min-h-0">{children}</div>
+  )
 }

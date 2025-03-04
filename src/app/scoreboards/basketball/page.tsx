46bb95ae6b8d7fa16df19a0ba16f0439314a88bb
Chanakarn Limprasertsiri <minklim47@gmail.com>
2025-03-05 00:48:05 +0700

    ✨ add score table and socket

--- src/app/scoreboards/basketball/page.tsx
+++ src/app/scoreboards/basketball/page.tsx
@@ -1,36 +1,98 @@
 'use client'
 
-import { Icon } from '@iconify/react/dist/iconify.js'
 import BasketBallCard from './components/BastketballCard'
-import { useRouter } from 'next/navigation'
+import { BasketballMatch } from './interfaces/basketball'
 import { mockBasketBallMatches } from './interfaces/basketball'
+// import { io } from 'socket.io-client'
+import { useEffect, useState } from 'react'
+import TableComponent from '@/shared/components/TableComponent'
+import TeamCell from '@/shared/components/TeamCell'
+import { Team } from '@/shared/utils/team'
+import BackButton from '@/shared/components/BackButton'
+
 const BasketBall = () => {
+  const [matches, setMatches] = useState<BasketballMatch[]>([])
+  useEffect(() => {
+    setMatches(mockBasketBallMatches);
+  }, []);
+  // useEffect(() => {
+  //   const socket = io(SOCKET_URL, {
+  //     transports: ['websocket'],
+  //     withCredentials: true,
+  //     path: SOCKET_PATH
+  //   })
 
-  const router = useRouter()
-  // const { matches, loading, error } = useFetchBasketballMatch();
+  //   socket.on('connect', () => {
+  //     console.log('✅ Connected to WebSocket:', socket.id)
+  //   })
 
-  // if (loading) return <p>Loading match data...</p>;
-  // if (error) return <p>Error: {error}</p>;
+  //   socket.on('updateScoreboard', (data) => {
+  //     console.log('📢 Received updateScoreboard event:', data)
+  //     if (data.length === 0) {
+  //       console.warn('⚠️ No match data received!')
+  //     }
+  //     setMatches(data)
+  //   })
+
+  //   socket.on('connect_error', (err) => {
+  //     console.error('❌ WebSocket Connection Error:', err)
+  //   })
+
+  //   return () => {
+  //     socket.disconnect()
+  //   }
+  // }, [])
+  // const SOCKET_URL = 'http://it3k-in.sit.kmutt.ac.th/'
+  // const SOCKET_PATH = '/api/basketball-service/socket/'
+
+  const header = ['อันดับ', 'TEAM', 'WIN_LOSE', 'POINT', 'POINT DIFF']
+  const tableData = [
+    [1, <TeamCell key="team-kmutt" team={Team.KMUTT} />, '0-0', '0-0', 0],
+    [1, <TeamCell key="team-kmitl" team={Team.KMITL} />, '0-0', '0-0', 0],
+    [1, <TeamCell key="team-kmutnb" team={Team.KMUTNB} />, '0-0', '0-0', 0],
+    [1, <TeamCell key="team-kmutnbpr" team={Team.KMUTNBPR} />, '0-0', '0-0', 0]
+  ]
+  const getColumnstyle = (colIndex: number) => {
+    if (colIndex === 0) return 'font-bold'
+    if (colIndex === 1) return 'w-1/3'
+    return ''
+  }
+
   return (
-    <div className=" flex flex-col w-full min-h-[calc(100vh-100px)] flex-1  items-center py-12  bg-black-300  space-y-2 sm:space-y-4 lg:space-y-8 font-Prompt bg-cover bg-bottom" style={{ backgroundImage: "url('/images/backgrounds/basketballBackground.png')" }} >
-      <div className="flex items-center w-full  pl-[5%] md:pl-[10%] mb-5">
-        <button onClick={() => router.push('/')}>
-          <Icon
-            icon={'solar:alt-arrow-left-bold'}
-            className="lg:text-6xl md:5xl text-4xl p-0 cursor-pointer text-white"
-          />
-        </button>
-        <h1 className="text-3xl text-white lg:text-4xl font-bold ">
+    <div
+      className=" flex flex-col w-full min-h-[calc(100vh-100px)] flex-1  items-center pb-12  bg-black-300 font-Prompt bg-cover bg-bottom"
+      style={{
+        backgroundImage: "url('/images/backgrounds/basketballBackground.png')"
+      }}>
+      <div className=" w-[90%] mt-8 mb-4 md:mb-8 md:mt-8 flex flex-row items-center lg:-ml-4 md:-ml-2 -ml-1 text-white ">
+        <BackButton />
+        <h1 className="text-2xl sm:text-3xl lg:text-4xl font-bold ">
           Basketball
         </h1>
       </div>
-      <div className='flex flex-col items-center w-full h-full space-y-4 sm:space-y-6 lg:space-y-8'>
-        {/* {matches?.map((match, index) => {
-          return <BasketBallCard data={match} key={index} />
-        })} */}
-         {mockBasketBallMatches?.map((match, index) => {
-          return <BasketBallCard data={match} key={index} />
-        })}
+      <div className="w-[90%]">
+        <TableComponent
+          highlighted={true}
+          edgeBorder={true}
+          header={header}
+          data={tableData}
+          columnStyles={getColumnstyle}
+        />
+      </div>
+      <p className="text-2xl sm:text-3xl lg:text-4xl font-bold text-white my-4 sm:my-8">
+        Opening Match
+      </p>
+      <div className="flex flex-col items-center w-full h-full space-y-4 sm:space-y-6 lg:space-y-8">
+        {/* {matches?.length === 0 ? (
+          <p className="text-white text-sm">Loading...</p>
+        ) : ( */}
+        {/* matches?.map((match, index) => (
+             <BasketBallCard data={match} key={index} />
+           )) */}
+        {matches?.map((match, index) => (
+          <BasketBallCard data={match} key={index} />
+        ))}
+        {/* )} */}
       </div>
     </div>
   )

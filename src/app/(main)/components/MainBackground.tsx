1235e2d46a110523187fe82780dd4cfd76f1564b
BoolinBood <napat.jins@mail.kmutt.ac.th>
2025-03-04 14:26:15 +0700

    🔧(main): adjust background image size

--- src/app/(main)/components/MainBackground.tsx
+++ src/app/(main)/components/MainBackground.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const MainBackground = () => {
   return (
-    <div className='absolute bottom-0 -z-10 w-[600px] sm:w-[1000px] sm:-bottom-[200px] h-fit x-fit left-1/2 -translate-x-1/2'>
+    <div className='absolute bottom-0 -z-10 w-[600px] sm:w-[1000px] md:w-full sm:-bottom-[200px] h-fit x-fit left-1/2 -translate-x-1/2'>
       <Image
         src="/images/backgrounds/main_background.png"
         alt="main background"

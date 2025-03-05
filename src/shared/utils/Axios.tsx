377b9f048fe7e06654e8f0ea206e3c02696e7867
Nannapat Petcharach <88127284+Nannapatx@users.noreply.github.com>
2025-03-06 02:04:45 +0700

    build the axios instance

---
+++ src/shared/utils/Axios.tsx
@@ -0,0 +1,13 @@
+import axios from 'axios'
+import { configure } from 'axios-hooks'
+
+export const BASE_URL = 'https://it3k.sit.kmutt.ac.th/api'
+
+const apiClient = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+configure({ axios: apiClient })

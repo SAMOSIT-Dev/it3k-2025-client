695a181f62e824ab5142cf4ec1ce3407da4e5278
Chanakarn Limprasertsiri <minklim47@gmail.com>
2025-03-06 15:20:59 +0700

    🐛 fix bugs

--- src/app/scoreboards/basketball/utils/formatTime.ts
+++ src/app/scoreboards/basketball/utils/formatTime.ts
@@ -1,27 +1,6 @@
-export const formatTime = (isoString: string, timezone: string = "Asia/Bangkok"): string => {
-    if (!isoString) return ""; 
-  
-    try {
-      const date = new Date(isoString);
-  
-      if (isNaN(date.getTime())) {
-        throw new Error(`Invalid date: ${isoString}`);
-      }
-  
-      const formattedTime = new Intl.DateTimeFormat('en-GB', {
-        hour: 'numeric',
-        minute: '2-digit',
-        hourCycle: 'h23', 
-        timeZone: timezone, 
-      }).format(date);
-  
-      return formattedTime.replace(":", ".");
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        console.error(`Error formatting time: ${error.message}`)
-      }
-      return "";
-    }
-  };
-  
-  
+export const formatTime = (isoString: string): string => {
+  const date = new Date(isoString)
+  const hours = date.getUTCHours().toString().padStart(2, '0')
+  const minutes = date.getUTCMinutes().toString().padStart(2, '0')
+  return `${hours}.${minutes}`
+}

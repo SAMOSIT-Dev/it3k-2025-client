9f8e3896a72b89b78af91c47bf55007889b764c0
Chanakarn Limprasertsiri <minklim47@gmail.com>
2025-03-06 15:20:46 +0700

    🐛 remove mock data

--- src/app/scoreboards/basketball/page.tsx
+++ src/app/scoreboards/basketball/page.tsx
@@ -1,10 +1,7 @@
 'use client'
 
 import BasketBallCard from './components/BastketballCard'
-// import { mockBasketBallMatches } from './interfaces/basketball'
 import TableComponent from '@/shared/components/TableComponent'
-// import TeamCell from '@/shared/components/TeamCell'
-// import { Team } from '@/shared/utils/team'
 import BackButton from '@/shared/components/BackButton'
 import { transformDashboardData } from './utils/transformTableData'
 import { useFetchBasketball } from './hooks/useFetchBasketball'
@@ -13,12 +10,7 @@ const BasketBall = () => {
   const { matches, entries } = useFetchBasketball()
 
   const header = ['อันดับ', 'TEAM', 'WIN_LOSE', 'POINT', 'POINT DIFF']
-  // const mockTableData = [
-  //   [1, <TeamCell key="team-kmutt" team={Team.KMUTT} />, '0-0', '0-0', 0],
-  //   [1, <TeamCell key="team-kmitl" team={Team.KMITL} />, '0-0', '0-0', 0],
-  //   [1, <TeamCell key="team-kmutnb" team={Team.KMUTNB} />, '0-0', '0-0', 0],
-  //   [1, <TeamCell key="team-kmutnbpr" team={Team.KMUTNB_PR} />, '0-0', '0-0', 0]
-  // ]
+
   const getColumnstyle = (colIndex: number) => {
     if (colIndex === 0) return 'font-bold'
     if (colIndex === 1) return 'w-1/3'
@@ -51,13 +43,6 @@ const BasketBall = () => {
             Loading ranking...
           </p>
         )}
-        {/* <TableComponent
-          highlighted={true}
-          edgeBorder={true}
-          header={header}
-          data={mockTableData}
-          columnStyles={getColumnstyle}
-        /> */}
       </div>
       <p className="text-2xl sm:text-3xl lg:text-4xl font-bold text-white my-4 sm:my-8">
         Opening Match
@@ -70,15 +55,11 @@ const BasketBall = () => {
               (a, b) =>
                 new Date(a.timeStart).getTime() -
                 new Date(b.timeStart).getTime()
-            ) 
+            )
             .map((match, index) => <BasketBallCard data={match} key={index} />)
         ) : (
           <p className="text-white text-sm py-10">Loading matches...</p>
         )}
-
-        {/* {mockBasketBallMatches.map((match, index) => (
-          <BasketBallCard data={match} key={index} />
-        ))} */}
       </div>
     </div>
   )
